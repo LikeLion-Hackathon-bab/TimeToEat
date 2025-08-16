9548164186003df8b538764adb8441be5a00df09
choikang <choikang7157@naver.com>
2025-08-16 20:46:06 +0900

    Refactor(#5): 빌드 에러 수정

--- src/main/java/com/example/timetoeat/global/config/SecurityConfig.java
+++ src/main/java/com/example/timetoeat/global/config/SecurityConfig.java
@@ -10,8 +10,6 @@ import com.example.timetoeat.global.auth.service.CustomOauthUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
