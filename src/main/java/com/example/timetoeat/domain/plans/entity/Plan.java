4f048bd6a8e787fe8b048533c77ccf355945572c
choikang <choikang7157@naver.com>
2025-08-25 19:51:36 +0900

    테스트

---
+++ src/main/java/com/example/timetoeat/domain/plans/entity/Plan.java
@@ -0,0 +1,27 @@
+package com.example.timetoeat.domain.plans.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false) private LocalDate meetingDate;
+    @Column(nullable = false) private LocalTime meetingTime;
+
+    @Column(nullable = false, length = 255)
+    private String location;
+
+    @Column(nullable = false, length = 100)
+    private String authorName;
+}
